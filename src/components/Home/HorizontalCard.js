ed84944e9a419c9a7d37e3e898e6efa1471c059b
mehdi-bhojani <m.mehdibhojani1@gmail.com>
2024-09-30 22:58:37 +0500

    updating what we offer loading

--- src/components/Home/HorizontalCard.js
+++ src/components/Home/HorizontalCard.js
@@ -4,11 +4,31 @@ import { Link } from "react-router-dom";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
 function HorizontalCard(props) {
+  const [loading, setLoading] = React.useState(true);
+
+  const handleLoadComplete = () => {
+    // console.log("Image has been loaded successfully!");
+    setLoading(false);
+  };
+
   return (
     <div>
       <div className="max-w-sm bg-[#AE9D95] rounded-lg shadow-lg w-full md:w-72 bg-opacity-[58%] md:mx-4 md:h-[100%]">
         <Link to={props.link}>
-          <LazyLoadImage className="rounded-t-lg h-[300px] object-cover" src={props.img} alt={props.title} effect="blur"/>
+        <div className="h-[300px] bg-gray-300 animate-pulse relative">
+          {loading && (
+              <img src="/logo.svg" alt="Loading placeholder" className="h-full w-full object-cover absolute" />
+              )}
+              <LazyLoadImage
+              className={`rounded-t-lg h-[300px] object-cover ${loading ? "hidden" : "block"}`}
+              src={props.img}
+              alt={props.title}
+              onLoad={handleLoadComplete}
+              delayTime={500}
+              effect="blur"
+              afterLoad={handleLoadComplete} // Another prop you can try
+              />
+              </div>
         </Link>
         <div className="p-5">
           <Link to={props.link}>
@@ -21,7 +41,7 @@ function HorizontalCard(props) {
           </p>
           <Link
             to={props.link}
-            className="inline-flex justify-center  px-3 py-2 text-sm font-medium text-center text-black bg-primary rounded-lg hover:bg-primary  focus:outline-none w-full"
+            className="inline-flex justify-center px-3 py-2 text-sm font-medium text-center text-black bg-primary rounded-lg hover:bg-primary focus:outline-none w-full"
           >
             Find Out More
           </Link>
